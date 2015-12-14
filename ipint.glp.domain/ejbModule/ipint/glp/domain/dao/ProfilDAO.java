c70dc368fde31c4bb2405879509a31cb4750bb9e
declerck <declerck@b12p7.fil.univ-lille1.fr>
2015-12-14 14:02:11 +0100

    RDE - Ajout ExperienceDAO et ProfilDAO

---
+++ ipint.glp.domain/ejbModule/ipint/glp/domain/dao/ProfilDAO.java
@@ -0,0 +1,40 @@
+package ipint.glp.domain.dao;
+
+import java.util.List;
+
+import ipint.glp.domain.entity.Profil;
+
+public class ProfilDAO extends DAO<Profil> {
+
+	@Override
+	public Profil find(Profil obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Profil create(Profil obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Profil ancienObj, Profil nouvelObj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Profil obj) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Profil> lister(Profil obj) {
+		return null;
+		// TODO Auto-generated method stub
+
+	}
+
+}
