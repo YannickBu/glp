b6935df8d1aa411fe018c00ee07d6b7b49229671
buchart <buchart@b12p11.fil.univ-lille1.fr>
2015-12-15 20:15:33 +0100

    RDE - Modif pour retirer le ArticleDAO

--- ipint.glp.domain/ejbModule/ipint/glp/domain/entity/Test.java
+++ ipint.glp.domain/ejbModule/ipint/glp/domain/entity/Test.java
@@ -1,20 +1,9 @@
 package ipint.glp.domain.entity;
 
-import java.util.GregorianCalendar;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
-
-import ipint.glp.domain.dao.ArticleDAO;
-import ipint.glp.domain.dao.ExperienceDAO;
-import ipint.glp.domain.dao.GroupeDAO;
-import ipint.glp.domain.dao.ProfilDAO;
-import ipint.glp.domain.dao.UtilisateurDAO;
-import ipint.glp.domain.entity.enumType.Statut;
 
 public class Test {
 
@@ -29,148 +18,107 @@ public class Test {
 
 	public static void main(String[] args) throws DatatypeConfigurationException {
 
-		// Les DAO
-		UtilisateurDAO utilisateurDao = new UtilisateurDAO();
-		ProfilDAO profilDao = new ProfilDAO();
-		ExperienceDAO experienceDao = new ExperienceDAO();
-		ArticleDAO articleDao = new ArticleDAO();
-		GroupeDAO groupeDao = new GroupeDAO();
-
-		//Test Create Utilisateur
-		Utilisateur utilisateur = new Utilisateur();
-		utilisateur.setNom("declerck");
-		utilisateur.setPrenom("rodolphe");
-		utilisateur.setEmail("rodolphe.declerck@gmail.com");
-		utilisateurDao.create(utilisateur);
-		
-		//Test Find Utilisateur
-		Utilisateur trouve = utilisateurDao.find(utilisateur);
-		System.out.println(trouve.getStatut());
-		
-		//Test Update Utilisateur
-		Utilisateur utilisateur2 = new Utilisateur();
-		utilisateur2.setStatut(Statut.DIPLOME);
-		utilisateurDao.update(utilisateur, utilisateur2);
-		System.out.println(trouve.getStatut());
-		
-		//Test Lister Utilisateur
-		for(Utilisateur u : utilisateurDao.lister()){
-			System.out.println(u.getNom());
-		}
-		
-		//Test Supprimer Utilisateur
-		//utilisateurDao.delete(utilisateur);
-		
-		//Test Create Profil
-		Profil profil = new Profil();
-		profil.setCursus("miage");
-		profil.setCompetence("Scrum");
-		profil.setCentreInteret("VolleyBall");
-		//TODO format telephone à voir
-		//profil.setTelephone("0659774852");
-		profilDao.create(profil);
-		
-		//Test Find Profil
-		Profil profilTrouve = profilDao.find(profil);
-		System.out.println(profilTrouve.getCentreInteret());
-		
-		//Test Update Profil
-		Profil profil2 = new Profil();
-		profil2.setCompetence("JAVA");
-		profilDao.update(profil, profil2);
-		System.out.println(profilTrouve.getCompetence());
-		
-		//Test Lister Profil
-		for(Profil p : profilDao.lister()){
-			System.out.println(p.getCursus());
-		}
-		
-		//Test Supprimer Utilisateur
-		//profilDao.delete(profil);
-		
-		//Test Create Experience
-		Experience experience = new Experience();
-		experience.setAnneeDebut(2012);
-		experience.setAnneFin(2012);
-		experience.setEntreprise("Capgemini");
-		experience.setPoste("Developpeur JAVA");
-		experience.setLieu("Lille");
-		experience.setDescription("developpement de site de suivi de projets");
-		experienceDao.create(experience);
-		
-		//Test Find Experience
-		Experience experienceTrouve = experienceDao.find(experience);
-		System.out.println(experienceTrouve.getEntreprise());
-		
-		//Test Update Experience
-		Experience experience2 = new Experience();
-		experience2.setEntreprise("CGI");
-		experienceDao.update(experience, experience2);
-		System.out.println(experienceTrouve.getEntreprise());
-		
-		//Test Lister Experience
-		for(Experience p : experienceDao.lister()){
-			System.out.println(p.getDescription());
-		}
-		
-		//Test Supprimer Experience
-		//experienceDao.delete(experience);
-		
-		//Test Create Article
-		Article article = new Article();
-		article.setUtilisateur(utilisateur);
-		GregorianCalendar datePublication = new GregorianCalendar(1993,10,27);
-        XMLGregorianCalendar xgcal = DatatypeFactory.newInstance()
-                        .newXMLGregorianCalendar(datePublication);
-		article.setDatePublication(datePublication);
-		article.setContenu("Hello world !");
-		articleDao.create(article);
-		
-		//Test Find Article
-		Article articleTrouve = articleDao.find(article);
-		System.out.println(articleTrouve.getDatePublication().getTime());
-		
-		//Test Update Article
-		Article article2 = new Article();
-		article2.setContenu("un article");
-		articleDao.update(article, article2);
-		System.out.println(articleTrouve.getContenu());
-		
-		//Test Lister Article
-		for(Article p : articleDao.lister()){
-			System.out.println(p.getDatePublication().getTime());
-			System.out.println(p.getUtilisateur().getEmail());
-		}
-		
-		//Test Supprimer Article
-		//articleDao.delete(article);
-		
-		
-		//Test Create Groupe
-		Groupe groupe = new Groupe();
-		groupe.setDescription("Groupe MIAGE FI");
-		groupe.setNomGroupe("MIAGE");
-		groupeDao.create(groupe);
-		
-		//Test Find Groupe
-		Groupe groupeTrouve = groupeDao.find(groupe);
-		System.out.println(groupeTrouve.getDescription());
-		
-		//Test Update Groupe
-		Groupe groupe2 = new Groupe();
-		groupe2.setDescription("Groupe MIAGE FA");;
-		groupeDao.update(groupe, groupe2);
-		System.out.println(groupeTrouve.getDescription());
-		
-		//Test Lister Groupe
-		for(Groupe p : groupeDao.lister()){
-			System.out.println(p.getNomGroupe());
-		}
-		
-		//Test Supprimer Groupe
-		//groupeDao.delete(groupe);
-		
-
+		/*
+		 * // Les DAO UtilisateurDAO utilisateurDao = new UtilisateurDAO();
+		 * ProfilDAO profilDao = new ProfilDAO(); ExperienceDAO experienceDao =
+		 * new ExperienceDAO(); ArticleDAO articleDao = new ArticleDAO();
+		 * GroupeDAO groupeDao = new GroupeDAO();
+		 * 
+		 * // Test Create Utilisateur Utilisateur utilisateur = new
+		 * Utilisateur(); utilisateur.setNom("declerck");
+		 * utilisateur.setPrenom("rodolphe");
+		 * utilisateur.setEmail("rodolphe.declerck@gmail.com");
+		 * utilisateurDao.create(utilisateur);
+		 * 
+		 * // Test Find Utilisateur Utilisateur trouve =
+		 * utilisateurDao.find(utilisateur);
+		 * System.out.println(trouve.getStatut());
+		 * 
+		 * // Test Update Utilisateur Utilisateur utilisateur2 = new
+		 * Utilisateur(); utilisateur2.setStatut(Statut.DIPLOME);
+		 * utilisateurDao.update(utilisateur, utilisateur2);
+		 * System.out.println(trouve.getStatut());
+		 * 
+		 * // Test Lister Utilisateur for (Utilisateur u :
+		 * utilisateurDao.lister()) { System.out.println(u.getNom()); }
+		 * 
+		 * // Test Supprimer Utilisateur // utilisateurDao.delete(utilisateur);
+		 * 
+		 * // Test Create Profil Profil profil = new Profil();
+		 * profil.setCursus("miage"); profil.setCompetence("Scrum");
+		 * profil.setCentreInteret("VolleyBall"); // TODO format telephone à
+		 * voir // profil.setTelephone("0659774852"); profilDao.create(profil);
+		 * 
+		 * // Test Find Profil Profil profilTrouve = profilDao.find(profil);
+		 * System.out.println(profilTrouve.getCentreInteret());
+		 * 
+		 * // Test Update Profil Profil profil2 = new Profil();
+		 * profil2.setCompetence("JAVA"); profilDao.update(profil, profil2);
+		 * System.out.println(profilTrouve.getCompetence());
+		 * 
+		 * // Test Lister Profil for (Profil p : profilDao.lister()) {
+		 * System.out.println(p.getCursus()); }
+		 * 
+		 * // Test Supprimer Utilisateur // profilDao.delete(profil);
+		 * 
+		 * // Test Create Experience Experience experience = new Experience();
+		 * experience.setAnneeDebut(2012); experience.setAnneFin(2012);
+		 * experience.setEntreprise("Capgemini"); experience.setPoste(
+		 * "Developpeur JAVA"); experience.setLieu("Lille");
+		 * experience.setDescription("developpement de site de suivi de projets"
+		 * ); experienceDao.create(experience);
+		 * 
+		 * // Test Find Experience Experience experienceTrouve =
+		 * experienceDao.find(experience);
+		 * System.out.println(experienceTrouve.getEntreprise());
+		 * 
+		 * // Test Update Experience Experience experience2 = new Experience();
+		 * experience2.setEntreprise("CGI"); experienceDao.update(experience,
+		 * experience2); System.out.println(experienceTrouve.getEntreprise());
+		 * 
+		 * // Test Lister Experience for (Experience p : experienceDao.lister())
+		 * { System.out.println(p.getDescription()); }
+		 * 
+		 * // Test Supprimer Experience // experienceDao.delete(experience);
+		 * 
+		 * // Test Create Article Article article = new Article();
+		 * article.setUtilisateur(utilisateur); GregorianCalendar
+		 * datePublication = new GregorianCalendar(1993, 10, 27);
+		 * XMLGregorianCalendar xgcal =
+		 * DatatypeFactory.newInstance().newXMLGregorianCalendar(datePublication
+		 * ); article.setDatePublication(datePublication); article.setContenu(
+		 * "Hello world !"); articleDao.create(article);
+		 * 
+		 * // Test Find Article Article articleTrouve =
+		 * articleDao.find(article);
+		 * System.out.println(articleTrouve.getDatePublication().getTime());
+		 * 
+		 * // Test Update Article Article article2 = new Article();
+		 * article2.setContenu("un article"); articleDao.update(article,
+		 * article2); System.out.println(articleTrouve.getContenu());
+		 * 
+		 * // Test Lister Article for (Article p : articleDao.lister()) {
+		 * System.out.println(p.getDatePublication().getTime());
+		 * System.out.println(p.getUtilisateur().getEmail()); }
+		 * 
+		 * // Test Supprimer Article // articleDao.delete(article);
+		 * 
+		 * // Test Create Groupe Groupe groupe = new Groupe();
+		 * groupe.setDescription("Groupe MIAGE FI");
+		 * groupe.setNomGroupe("MIAGE"); groupeDao.create(groupe);
+		 * 
+		 * // Test Find Groupe Groupe groupeTrouve = groupeDao.find(groupe);
+		 * System.out.println(groupeTrouve.getDescription());
+		 * 
+		 * // Test Update Groupe Groupe groupe2 = new Groupe();
+		 * groupe2.setDescription("Groupe MIAGE FA"); ; groupeDao.update(groupe,
+		 * groupe2); System.out.println(groupeTrouve.getDescription());
+		 * 
+		 * // Test Lister Groupe for (Groupe p : groupeDao.lister()) {
+		 * System.out.println(p.getNomGroupe()); }
+		 * 
+		 * // Test Supprimer Groupe // groupeDao.delete(groupe);
+		 */
 	}
 
 }
--- ipint.glp.domain/ejbModule/ipint/glp/domain/impl/ArticleImpl.java
+++ ipint.glp.domain/ejbModule/ipint/glp/domain/impl/ArticleImpl.java
@@ -1,41 +1,73 @@
 package ipint.glp.domain.impl;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.persistence.Query;
 
 import ipint.glp.api.DTO.ArticleDTO;
 import ipint.glp.api.itf.ArticleService;
-import ipint.glp.domain.dao.DAOFactory;
 import ipint.glp.domain.entity.Article;
 import ipint.glp.domain.entity.util.MappingToDTO;
 import ipint.glp.domain.entity.util.MappingToEntity;
 
 @Stateless
-public class ArticleImpl implements ArticleService	 {
+public class ArticleImpl implements ArticleService {
 
-	
+	@PersistenceContext(unitName = "PU", type = PersistenceContextType.EXTENDED)
+	private EntityManager em;
+
 	@Override
-	public ArticleDTO creerArticle(ArticleDTO obj) {
-		Article art = MappingToEntity.articleDTOToArticle(obj);
-		return MappingToDTO.articleToArticleDTO(DAOFactory.getArticleDAO().create(art));
+	public ArticleDTO creerArticle(ArticleDTO articleDTO) {
+		MappingToEntity mte = new MappingToEntity();
+		Article article = mte.articleDTOToArticle(articleDTO);
+		em.persist(article);
+
+		return articleDTO;
 	}
 
 	@Override
-	public ArticleDTO trouverArticle(ArticleDTO obj) {
-		Article art = MappingToEntity.articleDTOToArticle(obj);
-		return MappingToDTO.articleToArticleDTO(DAOFactory.getArticleDAO().find(art));
+	public ArticleDTO trouverArticle(ArticleDTO articleDTO) {
+		Query q = em.createQuery("select e from Article e where e.idArticle = '" + articleDTO.getIdArticle() + "'");
+		Article article = (Article) q.getSingleResult();
+		MappingToDTO mtd = new MappingToDTO();
+		articleDTO = mtd.articleToArticleDTO(article);
+		return articleDTO;
 	}
 
 	@Override
-	public ArticleDTO modifierArticle(ArticleDTO ancienObj, ArticleDTO nouvelObj) {
-		Article nouvelArt = MappingToEntity.articleDTOToArticle(ancienObj);
-		Article ancienArt = MappingToEntity.articleDTOToArticle(nouvelObj);
-		return MappingToDTO.articleToArticleDTO(DAOFactory.getArticleDAO().update(ancienArt, nouvelArt));
+	public ArticleDTO modifierArticle(ArticleDTO ancienArt, ArticleDTO nouvelArt) {
+		MappingToEntity mte = new MappingToEntity();
+		Article articleMAJ = mte.articleDTOToArticle(ancienArt);
+
+		if (nouvelArt.getContenu() != null) {
+
+			articleMAJ.setContenu(nouvelArt.getContenu());
+		}
+		if (nouvelArt.getDatePublication() != null) {
+
+			articleMAJ.setDatePublication(nouvelArt.getDatePublication());
+		}
+
+		em.persist(articleMAJ);
+		MappingToDTO mtd = new MappingToDTO();
+		nouvelArt = mtd.articleToArticleDTO(articleMAJ);
+
+		return nouvelArt;
 	}
 
 	@Override
-	public void supprimerArticle(ArticleDTO obj) {
-		Article art = MappingToEntity.articleDTOToArticle(obj);
-		DAOFactory.getArticleDAO().delete(art);
+	public void supprimerArticle(ArticleDTO articleASupprimer) {
+		MappingToEntity mte = new MappingToEntity();
+		Article article = mte.articleDTOToArticle(articleASupprimer);
+
+		if (articleASupprimer.getIdArticle() != null) {
+			em.remove(article);
+		}
+		// TODO gérer les exceptions pour le cas où le nom et l'id sont tous les
+		// deux à null
+
 	}
 
 }
