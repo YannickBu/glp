57eb57e1d5cca0ffde78e2357edfc073611c76ed
buchart <buchart@b12p11.fil.univ-lille1.fr>
2015-12-16 10:23:34 +0100

    YBU - mise en commentaire des DAO + config persistence

--- ipint.glp.domain/ejbModule/ipint/glp/domain/dao/ArticleDAO.java
+++ ipint.glp.domain/ejbModule/ipint/glp/domain/dao/ArticleDAO.java
@@ -11,84 +11,84 @@ import ipint.glp.domain.entity.Article;
 
 public class ArticleDAO extends DAO<Article> {
 
-	private static final String PERSISTENCE_UNIT_NAME = "PU";
-	private EntityManagerFactory emf;
-	public EntityManager em;
-
-	public ArticleDAO() {
-		emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-		em = emf.createEntityManager();
-	}
-
-	public Article update(final Article ancienArt, final Article nouvelArt) {
-
-		Article articleMAJ = find(ancienArt);
-
-		if (nouvelArt.getContenu() != null) {
-
-			articleMAJ.setContenu(nouvelArt.getContenu());
-		}
-		if (nouvelArt.getDatePublication() != null) {
-
-			articleMAJ.setDatePublication(nouvelArt.getDatePublication());
-		}
-		em.getTransaction().begin();
-
-		em.persist(articleMAJ);
-
-		em.getTransaction().commit();
-		
-		return articleMAJ;
-
-	}
-
-	@Override
-	public Article find(Article idArticle) {
-		em.getTransaction().begin();
-
-		Query q = em.createQuery("select e from Article e where e.idArticle = '" + idArticle.getIdArticle() + "'");
-		Article article = (Article) q.getSingleResult();
-
-		em.getTransaction().commit();
-
-		return article;
-	}
-
-	@Override
-	public Article create(Article article) {
-		em.getTransaction().begin();
-
-		em.persist(article);
-
-		em.getTransaction().commit();
-
-		return article;
-	}
-
-	@Override
-	public void delete(Article articleASupprimer) {
-		em.getTransaction().begin();
-		if (articleASupprimer.getIdArticle() != null) {
-			em.remove(articleASupprimer);
-		}
-		// TODO gérer les exceptions pour le cas où le nom et l'id sont tous les
-		// deux à null
-
-		em.getTransaction().commit();
-
-	}
-
-	@Override
-	public List<Article> lister() {
-		em.getTransaction().begin();
-
-		Query q = em.createQuery("select e from Article e");
-		List<Article> lesArticles = q.getResultList();
-
-		em.getTransaction().commit();
-
-		return lesArticles;
-
-	}
+//	private static final String PERSISTENCE_UNIT_NAME = "PU";
+//	private EntityManagerFactory emf;
+//	public EntityManager em;
+//
+//	public ArticleDAO() {
+//		emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+//		em = emf.createEntityManager();
+//	}
+//
+//	public Article update(final Article ancienArt, final Article nouvelArt) {
+//
+//		Article articleMAJ = find(ancienArt);
+//
+//		if (nouvelArt.getContenu() != null) {
+//
+//			articleMAJ.setContenu(nouvelArt.getContenu());
+//		}
+//		if (nouvelArt.getDatePublication() != null) {
+//
+//			articleMAJ.setDatePublication(nouvelArt.getDatePublication());
+//		}
+//		em.getTransaction().begin();
+//
+//		em.persist(articleMAJ);
+//
+//		em.getTransaction().commit();
+//		
+//		return articleMAJ;
+//
+//	}
+//
+//	@Override
+//	public Article find(Article idArticle) {
+//		em.getTransaction().begin();
+//
+//		Query q = em.createQuery("select e from Article e where e.idArticle = '" + idArticle.getIdArticle() + "'");
+//		Article article = (Article) q.getSingleResult();
+//
+//		em.getTransaction().commit();
+//
+//		return article;
+//	}
+//
+//	@Override
+//	public Article create(Article article) {
+//		em.getTransaction().begin();
+//
+//		em.persist(article);
+//
+//		em.getTransaction().commit();
+//
+//		return article;
+//	}
+//
+//	@Override
+//	public void delete(Article articleASupprimer) {
+//		em.getTransaction().begin();
+//		if (articleASupprimer.getIdArticle() != null) {
+//			em.remove(articleASupprimer);
+//		}
+//		// TODO gérer les exceptions pour le cas où le nom et l'id sont tous les
+//		// deux à null
+//
+//		em.getTransaction().commit();
+//
+//	}
+//
+//	@Override
+//	public List<Article> lister() {
+//		em.getTransaction().begin();
+//
+//		Query q = em.createQuery("select e from Article e");
+//		List<Article> lesArticles = q.getResultList();
+//
+//		em.getTransaction().commit();
+//
+//		return lesArticles;
+//
+//	}
 
 }
