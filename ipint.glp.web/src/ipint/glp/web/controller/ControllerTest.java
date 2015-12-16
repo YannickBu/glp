e76bfc5b522cabe6f159684653d890ba6b686bc9
buchart <buchart@b12p11.fil.univ-lille1.fr>
2015-12-16 14:57:05 +0100

    YBU - config serveur

--- ipint.glp.web/src/ipint/glp/web/controller/ControllerTest.java
+++ ipint.glp.web/src/ipint/glp/web/controller/ControllerTest.java
@@ -1,25 +1,31 @@
 package ipint.glp.web.controller;
 
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import ipint.glp.api.DTO.ArticleDTO;
+import ipint.glp.api.itf.ArticleService;
+
 @Controller
 public class ControllerTest {
 
-//	@Inject
-//	ArticleService s;
+	@Inject
+	ArticleService s;
 
 	public ControllerTest() {}
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public ModelAndView welcomeGet() {
-		//ArticleDTO art = new ArticleDTO();
-		//art.setContenu("du contenu");
-		//art = s.creerArticle(art);
-		//System.out.println(art.getContenu());
+		ArticleDTO art = new ArticleDTO();
+		art.setContenu("du contenu");
+		art = s.creerArticle(art);
+		System.out.println(art.getContenu());
+		System.out.println(art.getUtilisateur().getPrenom());
 		return new ModelAndView("accueil");
 	}
 	
