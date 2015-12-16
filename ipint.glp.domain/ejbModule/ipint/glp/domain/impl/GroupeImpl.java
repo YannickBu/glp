dadebe0389d64987102381420e8c04e98d4bc263
diagnem <diagnem@b12p6.fil.univ-lille1.fr>
2015-12-16 12:51:10 +0100

    [MDI] implementation GroupeImpl (creer et trouver)

---
+++ ipint.glp.domain/ejbModule/ipint/glp/domain/impl/GroupeImpl.java
@@ -0,0 +1,120 @@
+package ipint.glp.domain.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import ipint.glp.api.DTO.ArticleDTO;
+import ipint.glp.api.DTO.GroupeDTO;
+import ipint.glp.api.DTO.UtilisateurDTO;
+import ipint.glp.api.itf.GroupeService;
+import ipint.glp.domain.entity.Article;
+import ipint.glp.domain.entity.Groupe;
+import ipint.glp.domain.entity.Utilisateur;
+import ipint.glp.domain.entity.util.MappingToDTO;
+
+@Stateless
+public class GroupeImpl implements GroupeService {
+
+	@PersistenceContext(unitName = "PU")
+	private EntityManager em;
+
+	@Override
+	public GroupeDTO creer(GroupeDTO obj) {
+		Groupe groupe = new Groupe();
+		groupe.setNomGroupe(obj.getNomGroupe());
+		groupe.setDescription(obj.getDescription());
+		Utilisateur utilisateur = null;
+		if(obj.getUtilisateurResponsable().getIdUtilisateur() != null){
+			utilisateur = em.find(Utilisateur.class, obj.getUtilisateurResponsable().getIdUtilisateur());
+		}
+		else if(obj.getUtilisateurResponsable().getEmail() != null){
+			Query q = em.createQuery("select u from Utilisateur u where u.nom = '" + obj.getUtilisateurResponsable().getNom() + "'");
+			utilisateur = (Utilisateur) q.getSingleResult();
+		}
+		//TODO gérer les exceptions si id et email sont null
+		groupe.setUtilisateurResponsable(utilisateur);
+		List<Article> articles = new ArrayList<Article>();
+		if(obj.getArticles().size() > 0){
+			for(ArticleDTO articleDto : obj.getArticles()){
+				Article article = null;
+				if(articleDto.getIdArticle() !=  null){
+					article = em.find(Article.class, articleDto.getIdArticle());
+					articles.add(article);
+				}
+				//TODO gérer l'exception si id = null
+			}
+		}		
+		groupe.setArticles(articles);
+		List<Utilisateur> utilisateurs = new ArrayList<Utilisateur>();
+		if(obj.getUtilisateurs().size() > 0){
+			for(UtilisateurDTO utilisateurDto : obj.getUtilisateurs()){
+				Utilisateur u = null;
+				if(utilisateurDto.getIdUtilisateur() !=  null){
+					u = em.find(Utilisateur.class, utilisateurDto.getIdUtilisateur());
+					utilisateurs.add(u);
+				}
+				else if(utilisateurDto.getEmail() !=  null){
+					u = em.find(Utilisateur.class, utilisateurDto.getEmail());
+					utilisateurs.add(u);
+				}
+				//TODO gérer l'exception si id et email = null
+			}
+		}		
+		groupe.setUtilisateurs(utilisateurs);
+		em.persist(groupe);
+		Groupe groupeCreated = em.find(Groupe.class, groupe.getIdGroupe());
+		return MappingToDTO.groupeToGroupeDTO(groupeCreated);
+	}
+
+	@Override
+	public GroupeDTO trouverGroupe(GroupeDTO obj) {
+		if(obj.getIdGroupe() != null){
+			return MappingToDTO.groupeToGroupeDTO(em.find(Groupe.class, obj.getIdGroupe()));
+		}
+		else if(obj.getNomGroupe() != null){
+			return MappingToDTO.groupeToGroupeDTO(em.find(Groupe.class, obj.getNomGroupe()));
+		}
+		return null;
+	}
+
+	public GroupeDTO modifier(GroupeDTO ancienObj, GroupeDTO nouvelObj) {
+//		Groupe groupeMAJ = em.find(Groupe.class,ancienObj.getIdGroupe());
+//
+//		if (nouvelObj.getDescription() != null) {
+//			groupeMAJ.setDescription(nouvelObj.getDescription());
+//		}
+//		if (ancienObj.getUtilisateurResponsable() != null) {
+//
+//			groupeMAJ.setUtilisateurResponsable(nouvelObj.getUtilisateurResponsable());
+//		}
+//		em.getTransaction().begin();
+//
+//		em.persist(groupeMAJ);
+//
+//		em.getTransaction().commit();
+//
+//		return groupeMAJ;
+		//TODO
+		return null;
+	}
+
+	@Override
+	public GroupeDTO trouver(GroupeDTO obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void supprimer(GroupeDTO obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
