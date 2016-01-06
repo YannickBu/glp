e1fae492d2a6ee717cf5b9da4777e4e35f4e4cb1
diagnem <diagnem@b12p6.fil.univ-lille1.fr>
2016-01-06 10:39:19 +0100

    [MDI] ajout javadoc projet api

--- ipint.glp.api/src/ipint/glp/api/itf/ArticleService.java
+++ ipint.glp.api/src/ipint/glp/api/itf/ArticleService.java
@@ -4,15 +4,39 @@ import javax.ejb.Local;
 
 import ipint.glp.api.DTO.ArticleDTO;
 
+/**
+ * @author diagnem
+ *
+ */
 @Local
 public interface ArticleService {
 
+	/** Créer un article
+	 * @param article l'article à créer
+	 * @return l'article créé
+	 */
 	public ArticleDTO creer(ArticleDTO article);
 	
+	/** Trouver un article
+	 * @param article l'article à trouver
+	 * @return l'article trouvé
+	 */
 	public ArticleDTO trouver(ArticleDTO article);
 	
-	public ArticleDTO modifier(ArticleDTO articleAModifier);
+	/** Modifier un article
+	 * @param article l'article à modifier
+	 * @return l'article mopdifié
+	 */
+	/** Modifier un article
+	 * @param ancienArticle l'article à modifier
+	 * @param nouvelArticle le nouvel article avec les mises à jour
+	 * @return l'ancien article mis à jour
+	 */
+	public ArticleDTO modifier(ArticleDTO ancienArticle, ArticleDTO nouvelArticle);
 	
+	/** Supprimer
+	 * @param article l'article à supprimer
+	 */
 	public void supprimer(ArticleDTO article);
 	
 }
--- ipint.glp.api/src/ipint/glp/api/itf/UtilisateurService.java
+++ ipint.glp.api/src/ipint/glp/api/itf/UtilisateurService.java
@@ -7,12 +7,32 @@ import ipint.glp.api.DTO.UtilisateurDTO;
 @Local
 public interface UtilisateurService {
 
+	/** Créer un utilisateur
+	 * @param utilisateur l'utilisateur à créer
+	 * @return l'utilisateur créé
+	 */
 	public UtilisateurDTO creer(UtilisateurDTO utilisateur);
 	
+	/** Touver un utilisateur
+	 * @param utilisateur l'utilisateur à trouver
+	 * @return l'utilisateur trouvé
+	 */
 	public UtilisateurDTO trouver(UtilisateurDTO utilisateur);
 	
+	/** Modifier un utilisateur
+	 * @param utilisateur l'utilisateur à modifier
+	 * @return l'utilisateur modifié
+	 */
+	/** Modifier un utilisateur
+	 * @param ancienUtilisateur l'utilisateur à modifier
+	 * @param nouvelUtilisateur le nouvel utilisateur avec les mises à jour
+	 * @return l'ancien utilisateur mis à jour
+	 */
 	public UtilisateurDTO modifier(UtilisateurDTO ancienUtilisateur, UtilisateurDTO nouvelUtilisateur);
 	
+	/** Supprimer un utilisateur 
+	 * @param utilisateur l'utilisateur à supprimer
+	 */
 	public void supprimer(UtilisateurDTO utilisateur);
 	
 //	public List<UtilisateurDTO> listerUtilisateur();
