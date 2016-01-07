7c542feab6a3fd02c69e88beb1104696d7ce0129
barrois <barrois@b12p1.fil.univ-lille1.fr>
2016-01-07 14:38:35 +0100

    [MBA] test commit controllerTest

--- ipint.glp.web/src/ipint/glp/web/controller/ControllerTest.java
+++ ipint.glp.web/src/ipint/glp/web/controller/ControllerTest.java
@@ -38,115 +38,36 @@ public class ControllerTest {
 //		art = s.creerArticle(art);
 //		System.out.println(art.getContenu());
 //		System.out.println(art.getUtilisateur().getPrenom());
+		
+		
+		
+		
+		try{
+			ProfilDTO pDTO = new ProfilDTO();
+			pDTO.setCentreInteret("cent");
+			pDTO.setCompetence("comp");
+			pDTO.setCursus("cur");
+			pDTO.setTelephone("0311111111");
+			
+			UtilisateurDTO utilDTO = new UtilisateurDTO();
+			utilDTO.setEmail("yannick.buchart@g.com");
+			utilDTO.setNom("Buchart");
+			utilDTO.setPrenom("Yannick");
+			utilDTO.setPassword("psw");
+			utilDTO.setStatut(Statut.DIPLOME);
+			utilDTO.setProfil(pDTO);
+			
+			utilDTO = utilS.creer(utilDTO);
+			if(utilDTO!=null)
+				System.out.println("idUtil : " + utilDTO.getIdUtilisateur());
+				System.out.println(utilDTO.getEmail());
+			if(utilDTO.getProfil()!=null){
+				System.out.println(utilDTO.getProfil().getCentreInteret());
+			}
+		}
+		catch (Exception e){
+			
 
-//		
-//		ProfilDTO pDTO = new ProfilDTO();
-//		pDTO.setCentreInteret("cent");
-//		pDTO.setCompetence("comp");
-//		pDTO.setCursus("cur");
-//		pDTO.setTelephone("0311111111");
-//		
-//		UtilisateurDTO utilDTO = new UtilisateurDTO();
-//		utilDTO.setEmail("yannick.buchart@g.com");
-//		utilDTO.setNom("Buchart");
-//		utilDTO.setPrenom("Yannick");
-//		utilDTO.setPassword("psw");
-//		utilDTO.setStatut(Statut.DIPLOME);
-//		utilDTO.setProfil(pDTO);
-//		
-//		utilDTO = utilS.creer(utilDTO);
-//		if(utilDTO!=null)
-//			System.out.println("idUtil : " + utilDTO.getIdUtilisateur());
-//			System.out.println(utilDTO.getEmail());
-//		if(utilDTO.getProfil()!=null){
-//			System.out.println(utilDTO.getProfil().getCentreInteret());
-//		}
-		
-		//Creation utilisateur Manon
-		ProfilDTO pDTO = new ProfilDTO();
-		pDTO.setCentreInteret("Shopping, Cinema");
-		pDTO.setCompetence("JAVA 3; SQL 2; C++ 1");
-		pDTO.setCursus("");
-		pDTO.setTelephone("06.20.30.20.52");
-		List<String> dipl = new ArrayList<String>();
-		dipl.add("2015/2016 - M2MIAGE");
-		dipl.add("2012/2013 - L3MIAGE");
-		dipl.add("2010/2011 - DUT Informatique");
-		pDTO.setDiplomes(dipl);
-		
-		UtilisateurDTO utilDTO = new UtilisateurDTO();
-		utilDTO.setEmail("manon.barrois@gmail.com");
-		utilDTO.setNom("Barrois");
-		utilDTO.setPrenom("Manon");
-		utilDTO.setPassword("psw");
-		utilDTO.setStatut(Statut.DIPLOME);
-		GroupeDTO gDTO = new GroupeDTO();
-		gDTO.setNomGroupe("MIAGE");
-		gDTO = groupeS.trouver(gDTO);
-		GroupeDTO gDTO2 = new GroupeDTO();
-		gDTO2.setNomGroupe("SIAD");
-		gDTO2 = groupeS.trouver(gDTO2);		
-		List<GroupeDTO> grp = new ArrayList<GroupeDTO>();
-		grp.add(gDTO);
-		grp.add(gDTO2);
-		utilDTO.setGroupes(grp);
-		utilDTO.setProfil(pDTO);
-		
-		utilDTO = utilS.creer(utilDTO);
-		
-//		try{
-//			ProfilDTO pDTO = new ProfilDTO();
-//			pDTO.setCentreInteret("cent");
-//			pDTO.setCompetence("comp");
-//			pDTO.setCursus("cur");
-//			pDTO.setTelephone("0311111111");
-//			
-//			UtilisateurDTO utilDTO = new UtilisateurDTO();
-//			utilDTO.setEmail("yannick.buchart@g.com");
-//			utilDTO.setNom("Buchart");
-//			utilDTO.setPrenom("Yannick");
-//			utilDTO.setPassword("psw");
-//			utilDTO.setStatut(Statut.DIPLOME);
-//			utilDTO.setProfil(pDTO);
-//			
-//			utilDTO = utilS.creer(utilDTO);
-//			if(utilDTO!=null)
-//				System.out.println("idUtil : " + utilDTO.getIdUtilisateur());
-//				System.out.println(utilDTO.getEmail());
-//			if(utilDTO.getProfil()!=null){
-//				System.out.println(utilDTO.getProfil().getCentreInteret());
-//			}
-//		}
-//		catch (Exception e){
-//			
-//		}
-
-//		try{
-//			ProfilDTO pDTO = new ProfilDTO();
-//			pDTO.setCentreInteret("cent");
-//			pDTO.setCompetence("comp");
-//			pDTO.setCursus("cur");
-//			pDTO.setTelephone("0311111111");
-//			
-//			UtilisateurDTO utilDTO = new UtilisateurDTO();
-//			utilDTO.setEmail("yannick.buchart@g.com");
-//			utilDTO.setNom("Buchart");
-//			utilDTO.setPrenom("Yannick");
-//			utilDTO.setPassword("psw");
-//			utilDTO.setStatut(Statut.DIPLOME);
-//			utilDTO.setProfil(pDTO);
-//			
-//			utilDTO = utilS.creer(utilDTO);
-//			if(utilDTO!=null)
-//				System.out.println("idUtil : " + utilDTO.getIdUtilisateur());
-//				System.out.println(utilDTO.getEmail());
-//			if(utilDTO.getProfil()!=null){
-//				System.out.println(utilDTO.getProfil().getCentreInteret());
-//			}
-//		}
-//		catch (Exception e){
-//			
-//		}
 
 		return new ModelAndView("accueil");
 	}
