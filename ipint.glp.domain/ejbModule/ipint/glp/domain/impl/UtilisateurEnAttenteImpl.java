ba863dd975b873ef22ebc4f9234be7d6fb464053
declerck <declerck@b12p7.fil.univ-lille1.fr>
2016-01-08 11:38:52 +0100

    RDE - gestion conflit

--- ipint.glp.domain/ejbModule/ipint/glp/domain/impl/UtilisateurEnAttenteImpl.java
+++ ipint.glp.domain/ejbModule/ipint/glp/domain/impl/UtilisateurEnAttenteImpl.java
@@ -3,15 +3,19 @@ package ipint.glp.domain.impl;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
+import ipint.glp.api.DTO.UtilisateurDTO;
 import ipint.glp.api.DTO.UtilisateurEnAttenteDTO;
+import ipint.glp.api.DTO.enumType.Statut;
 import ipint.glp.api.itf.UtilisateurEnAttenteService;
-import ipint.glp.domain.entity.Groupe;
 import ipint.glp.domain.entity.UtilisateurEnAttente;
+import ipint.glp.domain.entity.util.GenererMotDePasse;
 import ipint.glp.domain.entity.util.MappingToDTO;
 
 /**
+ * Implementation des services liés à la gestion d'un utilisateur en attente de
+ * validation par un modérateur.
+ * 
  * @author declerck
  *
  */
@@ -22,19 +26,19 @@ public class UtilisateurEnAttenteImpl implements UtilisateurEnAttenteService {
 	private EntityManager em;
 
 	@Override
-	public UtilisateurEnAttenteDTO créer(UtilisateurEnAttenteDTO utilisateurEnAttenteDTO) {
+	public UtilisateurEnAttenteDTO creer(UtilisateurEnAttenteDTO utilisateurEnAttenteDTO) {
 		UtilisateurEnAttente utilisateurEnAttente = new UtilisateurEnAttente();
-		utilisateurEnAttente.setIdUtilisateurEnAttente(utilisateurEnAttenteDTO.getIdUtilisateurEnAttente());
 		utilisateurEnAttente.setDiplome(utilisateurEnAttenteDTO.getDiplome());
 		utilisateurEnAttente.setEmail(utilisateurEnAttenteDTO.getNom());
 		utilisateurEnAttente.setDateNaissance(utilisateurEnAttenteDTO.getDateNaissance());
-		Groupe groupe = null;
-		if (utilisateurEnAttenteDTO.getGroupePrincipal() != null) {
-			Query q = em.createQuery("select g from Groupe g where u.idGroupe = '"
-					+ utilisateurEnAttenteDTO.getGroupePrincipal().getIdGroupe() + "'");
-			groupe = (Groupe) q.getSingleResult();
-		}
-		utilisateurEnAttente.setGroupePrincipal(groupe);
+		// Groupe groupe = null;
+		// if (utilisateurEnAttenteDTO.getGroupePrincipal() != null) {
+		// Query q = em.createQuery("select g from Groupe g where u.idGroupe =
+		// '"
+		// + utilisateurEnAttenteDTO.getGroupePrincipal().getIdGroupe() + "'");
+		// groupe = (Groupe) q.getSingleResult();
+		// }
+		// utilisateurEnAttente.setGroupePrincipal(groupe);
 		utilisateurEnAttente.setAnneeDiplome(utilisateurEnAttenteDTO.getAnneeDiplome());
 		utilisateurEnAttente.setNom(utilisateurEnAttenteDTO.getPrenom());
 		utilisateurEnAttente.setPrenom(utilisateurEnAttenteDTO.getPrenom());
@@ -45,13 +49,25 @@ public class UtilisateurEnAttenteImpl implements UtilisateurEnAttenteService {
 	}
 
 	@Override
-	public void valider() {
-		// TODO Auto-generated method stub
+	public void supprimer(UtilisateurEnAttenteDTO utilisateurEnAttenteAValiderDTO) {
+		UtilisateurEnAttente utilisateurEnAttente = new UtilisateurEnAttente();
+		utilisateurEnAttente.setIdUtilisateurEnAttente(utilisateurEnAttenteAValiderDTO.getIdUtilisateurEnAttente());
+		em.remove(utilisateurEnAttente);
 	}
 
 	@Override
-	public void refuser() {
-		// TODO Auto-generated method stub
+	public void valider(UtilisateurEnAttenteDTO utilisateurEnAttenteAValiderDTO) {
+		UtilisateurDTO utilisateurDTO = new UtilisateurDTO();
+		utilisateurDTO.setEmail(utilisateurEnAttenteAValiderDTO.getEmail());
+		utilisateurDTO.setNom(utilisateurEnAttenteAValiderDTO.getNom());
+		utilisateurDTO.setPrenom(utilisateurEnAttenteAValiderDTO.getPrenom());
+		// TODO : Gérer date de naissance
+		utilisateurDTO.setStatut(Statut.DIPLOME);
+		// TODO : Gérer génération aléatoire du mot de passe
+		utilisateurDTO.setPassword("password");
+		UtilisateurImpl utilisateurService = new UtilisateurImpl();
+		utilisateurService.creer(utilisateurDTO);
+		supprimer(utilisateurEnAttenteAValiderDTO);
 	}
 
 }
