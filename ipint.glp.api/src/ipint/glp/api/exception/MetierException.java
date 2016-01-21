7edceb2713e471d83ba6d2c868df57f7122beb82
buchart <buchart@b12p11.fil.univ-lille1.fr>
2016-01-21 15:39:48 +0100

    YBU - Ajout des premieres exceptions + ajout de lentite UtilisateurEnAttente dans le persistence.xml

---
+++ ipint.glp.api/src/ipint/glp/api/exception/MetierException.java
@@ -0,0 +1,30 @@
+package ipint.glp.api.exception;
+
+public class MetierException extends Exception {
+	private static final long serialVersionUID = 7013811984706633458L;
+
+	public MetierException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MetierException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MetierException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MetierException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MetierException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ipint.glp.api/src/ipint/glp/api/exception/UtilisateurInconnu.java
@@ -0,0 +1,30 @@
+package ipint.glp.api.exception;
+
+public class UtilisateurInconnu extends MetierException {
+	private static final long serialVersionUID = -6037417485124562278L;
+
+	public UtilisateurInconnu() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UtilisateurInconnu(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UtilisateurInconnu(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UtilisateurInconnu(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public UtilisateurInconnu(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
